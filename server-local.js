85712b4bd34b26825b599c421f0b448422e42c50
pugongyingh <pgyhh@sina.cn>
2020-05-26 16:55:31 +0800

    Update server-local.js

--- server-local.js
+++ server-local.js
@@ -1,5 +1,13 @@
-'use strict';
 
-const app = require('./express/server');
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(80, function () {
+  console.log('Example app listening on port 3000!');
+});
